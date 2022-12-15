e9ffc5336cd3ca91f527a6f75bee32ecdea73f57
arya <gordra01@gmail.com>
2022-12-15 09:01:06 +0700

    reupload

--- global.R
+++ global.R
@@ -6,7 +6,6 @@ library(dplyr)  # for pipe operator & data manipulations
 library(plotly) # for data visualization and plots using plotly 
 library(ggplot2) # for data visualization & plots using ggplot2
 library(ggtext) # beautifying text on top of ggplot
-library(maps) # for USA states map - boundaries used by ggplot for mapping
 library(ggcorrplot) # for correlation plot
 library(shinycssloaders) # to add a loader while graph is populating
 
